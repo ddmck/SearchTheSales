1028e4221e30ac3b7c30a0cd838ec7b0da9f83ad
Donald McKendrick <ddmckendrick@gmail.com>
2014-11-02 16:20:44 +0000

    fix indentation from bricks

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -37,11 +37,13 @@ class StoresController < ApplicationController
   end
 
   private
-    def set_store
-      @store = Store.find(params[:id])
-    end
 
-    def store_params
-      params.require(:store).permit(:name, :url, :affiliate_code, :image_url)
-    end
+  def set_store
+    @store = Store.find(params[:id])
+  end
+
+  def store_params
+    params.require(:store).permit(:name, :url, :affiliate_code, :image_url)
+  end
+  
 end
