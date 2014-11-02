4461651f7fcf5d03783a88742ba065ff565a670e
Donald McKendrick <ddmckendrick@gmail.com>
2014-11-02 23:06:07 +0000

    Added wishlist section to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < ApplicationController
     @stores = Store.all
     @brands = Brand.all
     @products = Product.all
+    @wishlist = current_user.wishlist if current_user
   end
   
   def inside
