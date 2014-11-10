016da484a5154b7cc05553985f38736a1d130713
Donald McKendrick <ddmckendrick@gmail.com>
2014-11-10 14:51:16 +0000

    Stop being so strict about the url presence per store

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,5 @@
 class Store < ActiveRecord::Base
-  validates_presence_of :name, :url, :image_url
-  validates_uniqueness_of :name, :url
+  validates_presence_of :name
+  validates_uniqueness_of :name
   has_many :products
 end
