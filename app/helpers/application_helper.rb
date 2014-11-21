556fd7c65bcb92c7274314c5f768add1aff864ed
Donald McKendrick <ddmckendrick@gmail.com>
2014-11-21 14:21:50 +0000

    add assets helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
       @title = "#{value} | Searchthesales"
     end
   end
+
+  def asset_path(file_path)
+    if Rails.env.development?
+      "http://localhost:8000/" + file_path
+    elsif Rails.env.production?
+      "https://s3-eu-west-1.amazonaws.com/storeluv/" + file_path
+    end
+  end
 end
