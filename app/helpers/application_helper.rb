f1e346b54b7e76d6800fd602b6d89e4b74e12f08
Donald McKendrick <ddmckendrick@gmail.com>
2014-12-23 11:39:49 +0000

    add meta tag dynamicness, and show sizes in pop up modals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,17 @@
 module ApplicationHelper
-  def title(value)
-    unless value.nil?
-      @title = "#{value} | Searchthesales"
+  def title(title = nil)
+    if title.present?
+      content_for :title, title
+    else
+      content_for?(:title) ? APP_CONFIG['default_title'] + ' | ' + content_for(:title) : APP_CONFIG['default_title']
+    end
+  end
+
+  def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : APP_CONFIG['meta_description']
     end
   end
 
@@ -12,4 +22,6 @@ module ApplicationHelper
       "https://s3-eu-west-1.amazonaws.com/storeluv/" + file_path
     end
   end
+
+
 end
