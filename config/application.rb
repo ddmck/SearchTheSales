691141215ad2f3d291c9ed8f190031b23fbd5c9a
Donald McKendrick <ddmckendrick@gmail.com>
2015-01-16 11:33:02 +0000

    Improve CORS

--- config/application.rb
+++ config/application.rb
@@ -20,13 +20,10 @@ module Searchthesales
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.middleware.use Rack::Cors do
+    config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*',
-          :headers => :any,
-          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-          :methods => [:get, :post, :options, :delete, :put]
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
 
