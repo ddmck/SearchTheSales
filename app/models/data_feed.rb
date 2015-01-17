fc1c95f8a1f4494d843bfe5730bf3aaed02629f8
Donald McKendrick <ddmckendrick@gmail.com>
2015-01-17 10:37:03 +0000

    debug

--- app/models/data_feed.rb
+++ app/models/data_feed.rb
@@ -69,6 +69,7 @@ class DataFeed < ActiveRecord::Base
 
     if item[:reference_name].nil? || item[:image_url].nil? || item[:url].nil? || item[:brand].nil? || !detect_valid_url(item[:url])
       puts "####### NOT VALID MISSING IMPORTANT COLUMN!!!! ######"
+      puts "DataFeed: #{self.id}, Item: #{item}"
       return
     else
       product = Product.find_by_url(item[:url])
