a62c0c9bc7870a2c74c6f4687bb0c468ed193684
Donald McKendrick <ddmckendrick@gmail.com>
2015-01-17 11:26:21 +0000

    fix large image url taking over the image url key

--- app/models/data_feed.rb
+++ app/models/data_feed.rb
@@ -81,7 +81,7 @@ class DataFeed < ActiveRecord::Base
       product.name = set_name(product)
       product.description = item[:description]
       product.url = item[:url]
-      product.image_url = item[:image_url].gsub(/http:/, "https:")
+      product.image_url = item[:image_url].try(:gsub, "http:", "https:") || item[:large_image_url].try(:gsub, "http:", "https:")
       product.large_image_url = item[:large_image_url].try(:gsub, "http:", "https:") if item[:large_image_url]
       product.colors = set_colors(item)
       product.gender = set_gender(item)
