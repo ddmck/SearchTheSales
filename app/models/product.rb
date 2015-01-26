21e3eef71d97e38c62f20de26ea3ec90a9c2b50d
Donald McKendrick <ddmckendrick@gmail.com>
2015-01-26 14:30:13 +0000

    make sure to remove document on delete call

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,6 +14,10 @@ class Product < ActiveRecord::Base
     delete_document
   end
 
+  after_commit on: [:delete] do
+    delete_document
+  end
+
   validates_presence_of :name, :brand_id, :store_id, :url
   validates_uniqueness_of :name, :url
   belongs_to :brand
