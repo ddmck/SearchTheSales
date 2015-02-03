6b27c70f4b110e8b62ab105e0288ec5c5e91be8b
Grantlyk <grant_leadbetter@live.co.uk>
2015-02-03 10:12:58 +0000

    Added script file for topshop

---
+++ lib/image_assets/topshop.rb
@@ -0,0 +1,38 @@
+class Topshop
+
+
+  def self.generate_image_urls(p)
+    image_urls = []
+    
+    base_url = p.image_url
+
+    image_urls << main_to_zm(base_url)
+
+    (1..7).each do |i|
+      image_urls << main_to_zm_numb(base_url, i)
+    end
+
+    return image_urls 
+  end
+
+  def self.main_to_zm_numb(url, count)
+    return url.to_s.gsub(/_main/, "m#{count}_zm")
+  end
+
+  def self.main_to_zm(url)
+    return url.gsub(/_main/, '_zm').to_s
+  end
+
+    s = Store.find_by_name("topshop")
+
+    prod = s.products
+
+    cat = Category.find_by_name("bags")
+    puts cat
+
+    prod.each do |p|
+      image_urls = generate_image_urls(p)
+      p.image_urls = image_urls
+      p.save
+    end
+end
