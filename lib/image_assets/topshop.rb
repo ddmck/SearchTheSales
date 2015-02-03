89b39948942fc7753fddd1383a914a500b383903
Grantlyk <grant_leadbetter@live.co.uk>
2015-02-03 11:39:04 +0000

    Finished off the topshop script

--- lib/image_assets/topshop.rb
+++ lib/image_assets/topshop.rb
@@ -1,38 +1,45 @@
 class Topshop
-
-
-  def self.generate_image_urls(p)
-    image_urls = []
-    
-    base_url = p.image_url
-
-    image_urls << main_to_zm(base_url)
-
-    (1..7).each do |i|
-      image_urls << main_to_zm_numb(base_url, i)
-    end
-
-    return image_urls 
-  end
-
-  def self.main_to_zm_numb(url, count)
-    return url.to_s.gsub(/_main/, "m#{count}_zm")
-  end
-
-  def self.main_to_zm(url)
-    return url.gsub(/_main/, '_zm').to_s
-  end
-
+  def self.import
     s = Store.find_by_name("topshop")
 
     prod = s.products
-
-    cat = Category.find_by_name("bags")
-    puts cat
 
     prod.each do |p|
       image_urls = generate_image_urls(p)
       p.image_urls = image_urls
       p.save
     end
+  end
+  def self.generate_image_urls(p)
+    image_urls = []
+    base_url = p.image_url
+
+    bags = Category.find_by_name("bags")
+    shoes = Category.find_by_name("shoes")
+    accessories = Category.find_by_name("accessories")
+
+    image_urls << normal_to_large(base_url)
+
+    if p.category_id == accessories.id
+      return image_urls
+    elsif p.category_id == bags.id || p.category_id == shoes.id
+      (2..3).each do |i|
+        image_urls << extract_images(base_url, i)
+      end
+      return image_urls
+    else 
+      (2..4).each do |i|
+        image_urls << extract_images(base_url, i)
+      end
+      return image_urls 
+    end
+  end
+
+  def self.normal_to_large(url)
+    return url.gsub(/_normal/, '_large').to_s
+  end
+
+  def self.extract_images(url, count)
+    return url.gsub(/_normal/, "_#{count}_large")
+  end
 end
