638e1456bd0e6f2b11744846a9e35f976ab7d4a2
Grantlyk <grant_leadbetter@live.co.uk>
2015-02-04 10:02:15 +0000

    Added working tests for french connection script

--- spec/image_assets/french_connection_spec.rb
+++ spec/image_assets/french_connection_spec.rb
@@ -1,1 +1,9 @@
-french_connection_spec.rb
+require 'rails_helper'
+require 'french_connection'
+
+RSpec.describe "schuh" do
+    it 'When url is sanitized it is sanitized correctly' do
+      expect(FrenchConnection.model_to_blank("www.test.com/_model")).to eq("www.test.com/")
+      expect(FrenchConnection.model_to_numb("www.test.com/_model", 3)).to eq("www.test.com/_3") 
+    end
+end
