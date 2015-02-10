ec12867fadce5e0f06b1b20ab4ed42be96d86e0f
Donald McKendrick <ddmckendrick@gmail.com>
2015-02-10 15:43:11 +0000

    fix weird lag on some requests with unicorn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,21 +35,13 @@ class ApplicationController < ActionController::Base
   end
 
   def get_current_user
-    puts "cookies: #{}"
-    puts request.headers['access-token']
-    cookies.each {|k| puts k}
-    puts "tokens: #{cookies[:auth_headers]}"
     return nil unless cookies[:auth_headers]
     auth_headers = JSON.parse cookies[:auth_headers]
-    puts "Auth headers: "
-    puts auth_headers
     expiration_datetime = DateTime.strptime(auth_headers["expiry"], "%s")
     current_user = User.find_by uid: auth_headers["uid"]
-
     if current_user &&
        current_user.tokens.has_key?(auth_headers["client"]) &&
        expiration_datetime > DateTime.now
-      puts "current_user: #{current_user}"
       @current_user = current_user
     end
     @current_user
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,9 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy, :buy, :wish]
   before_action :get_collections, only: [:new, :edit]
-  skip_before_filter :verify_authenticity_token
-  # before_action :authenticate_current_user, only: [:wish]
   respond_to :html, :json
 
   def index
-    puts "User:  #{get_current_user}"
-    puts "Params:  #{params}"
-    # if params[:search_string]
     hash = {query: {
                     query_string: {
                       default_field: "reference_name",
@@ -20,29 +15,6 @@ class ProductsController < ApplicationController
       hash[:sort] = [{args[0] => args[1]}]
     end
     @products = Product.__elasticsearch__.search(hash).page(params[:page]).records
-    # else
-    #   if params["gender"]
-    #     @gender = Gender.find_by_name(params["gender"])
-    #     if params[:category]
-    #       @products = Product.where(category_id: params[:category], gender_id: @gender.id)
-    #       if params[:sub_category]
-    #         @products = @products.where(sub_category_id: params[:sub_category])
-    #       end
-    #     else
-    #       @products = @gender.products
-    #     end
-    #   elsif params[:category]
-    #     @category = Category.find(params[:category])
-    #     @products = @category.products
-    #     if params[:sub_category]
-    #       @products = @products.where(sub_category_id: params[:sub_category])
-    #     end
-    #   else  
-    #     @products = Product.all
-        
-    #   end
-    #   @products = @products.paginate(page: params[:page])
-    # end
     respond_with(@products)
   end
 
