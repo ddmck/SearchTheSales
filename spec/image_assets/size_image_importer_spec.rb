fda7b90e6f54c832524e02cf70583b2e9950cc80
Grantlyk <grant_leadbetter@live.co.uk>
2015-02-13 13:54:31 +0000

    Fixing these tests cause tests are king

--- spec/image_assets/size_image_importer_spec.rb
+++ spec/image_assets/size_image_importer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "size" do
     it 'When url is sanitized it is sanitized correctly' do
-      expect(SizeImageImporter.change_char_value("http://i1.adis.ws/i/jpl/sz_170041_a?w=370&h=415&qlt=80&unsharp=0,1,1,7&img404=sz_imagemissing", "b")).to eq("http://i1.adis.ws/i/jpl/sz_170041_b?w=370&h=415&qlt=80&unsharp=0,1,1,7&img404=sz_imagemissing")
-      expect(SizeImageImporter.change_char_value("http://i1.adis.ws/i/jpl/sz_170041_a?w=370&h=415&qlt=80&unsharp=0,1,1,7&img404=sz_imagemissing", "c")).to eq("http://i1.adis.ws/i/jpl/sz_170041_c?w=370&h=415&qlt=80&unsharp=0,1,1,7&img404=sz_imagemissing")
+      expect(SizeImageImporter.new.change_char_value("http://i1.adis.ws/i/jpl/sz_170041_a?w=370&h=415&qlt=80&unsharp=0,1,1,7&img404=sz_imagemissing", "b")).to eq("http://i1.adis.ws/i/jpl/sz_170041_b?w=370&h=415&qlt=80&unsharp=0,1,1,7&img404=sz_imagemissing")
+      expect(SizeImageImporter.new.change_char_value("http://i1.adis.ws/i/jpl/sz_170041_a?w=370&h=415&qlt=80&unsharp=0,1,1,7&img404=sz_imagemissing", "c")).to eq("http://i1.adis.ws/i/jpl/sz_170041_c?w=370&h=415&qlt=80&unsharp=0,1,1,7&img404=sz_imagemissing")
     end
 end
