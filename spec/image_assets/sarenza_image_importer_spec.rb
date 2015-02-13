b0796ae2b551d98e5d9ad72ec8a67034d76ea713
Grantlyk <grant_leadbetter@live.co.uk>
2015-02-13 16:17:39 +0000

    Added sarenza tests

---
+++ spec/image_assets/sarenza_image_importer_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "sarenza" do
+    it 'When url is sanitized it is sanitized correctly' do
+      expect(SarenzaImageImporter.new.get_imgage_url("http://cdn.sarenza.net/static/_img/productsV4/0000029417/HD_0000029417_186958_09.jpg", 4)).to eq("http://cdn.sarenza.net/static/_img/productsV4/0000029417/HD_0000029417_186958_04.jpg")
+    end
+end
