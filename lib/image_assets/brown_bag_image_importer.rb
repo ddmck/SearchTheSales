ddb8a3222bcb4d08a058ff91ba918ff3963310f9
Grantlyk <grant_leadbetter@live.co.uk>
2015-02-13 14:41:47 +0000

    Wrote a brown bag script

---
+++ lib/image_assets/brown_bag_image_importer.rb
@@ -0,0 +1,30 @@
+class BrownBagImageImporter
+  def import
+    s = Store.find_by_name("BB Clothing")
+
+    prod = s.products.where(image_urls: nil)
+
+    prod.each do |p|
+      image_urls = generate_image_urls(p)
+      p.image_urls = image_urls
+      p.save
+    end
+  end
+  handle_asynchronously :import, :queue => 'data_feeds'
+
+  def generate_image_urls(p)
+    image_urls = []
+    
+    base_url = p.image_url
+
+    (1..4).each do |i|
+      image_urls << grab_image_url(base_url, i)
+    end
+
+    return image_urls 
+  end
+
+  def grab_image_url(url, count)
+    return url.to_s.gsub(/_1/, "_#{count}")
+  end
+end
