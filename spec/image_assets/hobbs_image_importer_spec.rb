c2000dc7684e312a8f602a4e93053712b014fef5
Grantlyk <grant_leadbetter@live.co.uk>
2015-02-13 10:52:07 +0000

    Corrected hobbs image importer spec

--- spec/image_assets/hobbs_image_importer_spec.rb
+++ spec/image_assets/hobbs_image_importer_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "schuh" do
+RSpec.describe "hobbs" do
     it 'When url is sanitized it is sanitized correctly' do
       expect(HobbsImageImporter.new.extract_images("www.test.com/_01_", 2)).to eq("www.test.com/_02_")
     end
