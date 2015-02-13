b3d21c15746a0a7e81e121f3442306489d1788dc
Grantlyk <grant_leadbetter@live.co.uk>
2015-02-13 10:51:56 +0000

    Corrected french connection spec file

--- spec/image_assets/french_connection_image_importer_spec.rb
+++ spec/image_assets/french_connection_image_importer_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "schuh" do
+RSpec.describe "french_connection" do
     it 'When url is sanitized it is sanitized correctly' do
       expect(FrenchConnectionImageImporter.new.model_to_blank("www.test.com/_model")).to eq("www.test.com/")
       expect(FrenchConnectionImageImporter.new.model_to_numb("www.test.com/_model", 3)).to eq("www.test.com/_3")
