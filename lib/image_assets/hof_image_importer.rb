3e691b50267beae0b1c24c14ab6c0a18097e56fc
Grantlyk <grant_leadbetter@live.co.uk>
2015-02-24 10:50:27 +0000

    Added hot image importer

---
+++ lib/image_assets/hof_image_importer.rb
@@ -0,0 +1,30 @@
+class HOFImageImporter
+  def import
+    s = Store.find(4)
+
+    prod = s.products.where(image_urls: nil)
+
+    prod.each do |p|
+      image_urls = generate_image_urls(p)
+      p.image_urls = image_urls
+      p.save
+    end
+  end
+  handle_asynchronously :import, :queue => 'data_feeds'
+
+  def generate_image_urls(p)
+    image_urls = []
+    
+    base_url = p.image_url
+
+    (0..4).each do |i|
+      image_urls << grab_image_url(base_url, i)
+    end
+
+    return image_urls 
+  end
+
+  def grab_image_url(url, count)
+    return url.to_s.gsub(/_00_/, "_0#{count}_")
+  end
+end
