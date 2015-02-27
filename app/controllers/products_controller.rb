3f95baff7e59cc24b46d30cac0927591dee53328
Donald McKendrick <ddmckendrick@gmail.com>
2015-02-27 15:34:45 +0000

    add brand filtering

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -107,6 +107,7 @@ class ProductsController < ApplicationController
     string = string.downcase.split(" ").join("^2 ") + '^2' + ' OR ' + string.downcase.split(" ").join("~1 ") + '~1' 
     string += ' AND category_id: ' + params[:category] if params[:category]
     string += ' AND sub_category_id: ' + params[:sub_category]  if params[:sub_category]
+    string += ' AND brand_id: ' + params[:brand] if params[:brand]
     if params[:gender]
       @gender = Gender.find_by_name(params[:gender])
       string += " AND gender_id: #{@gender.id}"
