6818f553c3886617d6d4137a1afc94061cad39bd
Grantlyk <grant_leadbetter@live.co.uk>
2015-03-25 17:36:56 +0000

    Updated brands model to incude a script that generates an array containing the three categories with the most products for that brand

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -7,5 +7,38 @@ class Brand < ActiveRecord::Base
   friendly_id :name, use: :slugged
 
   scope :featured, -> { where(featured: true)}
+  serialize :featured_categories
+
+  def self.generate_all
+    self.featured.each { |b| b.generate_featured_categories }
+  end
+
+  def generate_featured_categories
+    products = self.products
+    categories = Category.all
+    categoriesHash = Hash.new
+    categoriesArr = []
+    holdingArr = []
+    lastArr = []
+
+
+    categories.each do |c|
+      categoriesHash[c.id] = products.where(category_id: c.id).count
+    end
+
+    categoriesArr = categoriesHash.sort_by { |id, count| count }.reverse
+
+    (0..2).each do |count|
+      holdingArr << categoriesArr[count][0]
+    end
+
+    holdingArr.each do |id|
+      Category.find(id).name
+      lastArr << "#{id}-" + Category.find(id).name
+    end
+
+    self.featured_categories = lastArr
+    self.save
+  end
 
 end
