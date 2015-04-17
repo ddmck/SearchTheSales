cbdbde93c732b1ff6fbbeefba502edc99c938994
Donald McKendrick <ddmckendrick@gmail.com>
2015-04-17 15:38:20 +0100

    change products config default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Searchthesales::Application.configure do
 
   # Mailer
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: request.host_with_port }
+  config.action_mailer.default_url_options = { host: lambda { |env| Rack::Request.new(env).host } }
   config.action_mailer.delivery_method = :mailgun
   # config.action_mailer.smtp_settings = {
   #   address: ENV['SMTP_SERVER'],
