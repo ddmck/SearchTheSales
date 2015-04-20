4fdc231170dc53fda515856f8b6546a336de99e1
Grant Leadbetter <grant@nespite.io>
2015-04-20 16:14:17 +0100

    Updated new user mailer

--- app/mailers/new_user_mailer.rb
+++ app/mailers/new_user_mailer.rb
@@ -2,14 +2,26 @@ class NewUserMailer
 
   def initialize(user)
     @mg_client = Mailgunner::Client.new()
-    @html =  render_anywhere('mailgun_templates/welcome', {:email => user.email}).to_str
-    @params = {
-      :from    => "Bertie Wilson <customercare@fetchmyfashion.com>",
-      :to      => user.email,
-      :subject => 'Welcome to Fetch My Fashion!',
-      :text    => 'Welcome to Fetch My Fashion!', 
-      :html    => @html
-    }
+
+    if user.fetch_my_fashion
+      @html =  render_anywhere('mailgun_templates/fetchmyfashion/welcome', {:email => user.email}).to_str
+      @params = {
+        :from    => "Bertie Wilson <customercare@fetchmyfashion.com>",
+        :to      => user.email,
+        :subject => 'Welcome to Fetch My Fashion!',
+        :text    => 'Welcome to Fetch My Fashion!', 
+        :html    => @html
+      }
+    else
+      @html =  render_anywhere('mailgun_templates/searchthesales/welcome', {:email => user.email}).to_str
+      @params = {
+        :from    => "Bertie Wilson <customercare@searchthesales.com>",
+        :to      => user.email,
+        :subject => 'Welcome to Search The Sales!',
+        :text    => 'Welcome to Search The Sales!', 
+        :html    => @html
+      }
+    end
   end
 
   def render_anywhere(partial, assigns = {})
