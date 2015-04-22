2f09cfb87357311c6a6030ff5bf39ba4e0d9c0c2
Grant Leadbetter <grant@nespite.io>
2015-04-22 17:05:09 +0100

    Fixed a bug in the image assets spec for UrbanOutfitters

--- spec/image_assets/urban_outfitters_image_importer_spec.rb
+++ spec/image_assets/urban_outfitters_image_importer_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "urban_outfitters" do
     it 'When url is sanitized it is sanitized correctly' do
-      expect(UrbanOutfittersImageImporter.new.change_char_value("http://euimages.urbanoutfitters.com/is/image/UrbanOutfittersEU/5752468611234_001_a?$detailMain$", "e")).to eq("http://euimages.urbanoutfitters.com/is/image/UrbanOutfittersEU/5752468611234_001_e?$detailMain$")
+      expect(UrbanOutfittersImageImporter.new.change_char_value("http://euimages.urbanoutfitters.com/is/image/UrbanOutfittersEU/5752468611234_001_b?$detailMain$", "e")).to eq("http://euimages.urbanoutfitters.com/is/image/UrbanOutfittersEU/5752468611234_001_e?$detailMain$")
     end
 end
