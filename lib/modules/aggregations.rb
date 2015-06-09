90fca547e78b65e61b81a70c395a7feb6bdaff87
Grant Leadbetter <grant@nespite.io>
2015-06-09 16:19:15 +0100

    Aggregations module contains methods related to aggregation for elasticsearch

---
+++ lib/modules/aggregations.rb
@@ -0,0 +1,42 @@
+module Aggregations
+	def build_aggs_result(hash)
+    brands = build_from_aggs(hash[:brands][:buckets], Brand)
+    categories = build_from_aggs(hash[:categories][:buckets], Category)
+    styles = build_from_aggs(hash[:styles][:buckets], Style)
+    result = ["brands" => brands, "categories" => categories, "styles" => styles]
+    result
+  end
+
+  def build_from_aggs(items, klass)
+    array = []
+    items.each do |item|
+      b = klass.find(item[:key])
+      array << ["id" => b.id, "count" => item[:doc_count], "name" => b.name] 
+    end
+    array
+  end
+
+  def build_aggs
+	  {brands: {
+	    terms: { field: "brand_id" }
+	  },
+	  colors: {
+	    terms: { field: "color_id" }
+	  },
+	  categories: {
+	    terms: { field: "category_id" }
+	  },
+	  subCategories: {
+	    terms: { field: "sub_category_id" }
+	  },
+	  genders: {
+	    terms: { field: "gender_id" }
+	  },
+	  styles: {
+	    terms: { field: "style_id" }
+	  },
+	  material: {
+	    terms: { field: "material_id" }
+	  }}
+  end
+end
