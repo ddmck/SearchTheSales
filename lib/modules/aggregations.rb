48a8ee4eb1ce7a26a0dd0c80ef1bb8e3ae9ebf0e
Grant Leadbetter <grant@nespite.io>
2015-06-10 17:48:15 +0100

    added nil checking to aggregations

--- lib/modules/aggregations.rb
+++ lib/modules/aggregations.rb
@@ -1,11 +1,11 @@
 module Aggregations
 	def build_aggs_result(hash)
-    brands = build_from_aggs(hash[:brands][:buckets], Brand)
-    colors = build_from_aggs(hash[:colors][:buckets], Color)
-    categories = build_from_aggs(hash[:categories][:buckets], Category)
-    sub_categories = build_from_aggs(hash[:subCategories][:buckets], SubCategory)
-    styles = build_from_aggs(hash[:styles][:buckets], Style)
-    materials = build_from_aggs(hash[:materials][:buckets], Material)
+    brands = build_from_aggs(hash[:brands][:buckets], Brand) if hash[:brands]
+    colors = build_from_aggs(hash[:colors][:buckets], Color) if hash[:colors]
+    categories = build_from_aggs(hash[:categories][:buckets], Category) if hash[:categories]
+    sub_categories = build_from_aggs(hash[:subCategories][:buckets], SubCategory) if hash[:subCategories]
+    styles = build_from_aggs(hash[:styles][:buckets], Style) if hash[:styles]
+    materials = build_from_aggs(hash[:materials][:buckets], Material) if hash[:materials]
     {"brands" => brands, "colors" => colors, "categories" => categories, "sub_categories" => sub_categories, "styles" => styles, "materials" => materials}
   end
 
