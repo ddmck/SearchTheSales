b7e3d0555dc62e0594bf5123c5f2dabe5a699572
Grant Leadbetter <grant@nespite.io>
2015-06-16 16:46:12 +0100

    added if to fix nil error with hash

--- lib/modules/aggregations.rb
+++ lib/modules/aggregations.rb
@@ -1,12 +1,14 @@
 module Aggregations
 	def build_aggs_result(hash)
-    brands = build_from_aggs(hash[:brands][:buckets], Brand) if hash[:brands]
-    colors = build_from_aggs(hash[:colors][:buckets], Color) if hash[:colors]
-    categories = build_from_aggs(hash[:categories][:buckets], Category) if hash[:categories]
-    sub_categories = build_from_aggs(hash[:subCategories][:buckets], SubCategory) if hash[:subCategories]
-    styles = build_from_aggs(hash[:styles][:buckets], Style) if hash[:styles]
-    materials = build_from_aggs(hash[:materials][:buckets], Material) if hash[:materials]
-    {"brands" => brands, "colors" => colors, "categories" => categories, "sub_categories" => sub_categories, "styles" => styles, "materials" => materials}
+		if hash
+	    brands = build_from_aggs(hash[:brands][:buckets], Brand) if hash[:brands]
+	    colors = build_from_aggs(hash[:colors][:buckets], Color) if hash[:colors]
+	    categories = build_from_aggs(hash[:categories][:buckets], Category) if hash[:categories]
+	    sub_categories = build_from_aggs(hash[:subCategories][:buckets], SubCategory) if hash[:subCategories]
+	    styles = build_from_aggs(hash[:styles][:buckets], Style) if hash[:styles]
+	    materials = build_from_aggs(hash[:materials][:buckets], Material) if hash[:materials]
+	    return {"brands" => brands, "colors" => colors, "categories" => categories, "sub_categories" => sub_categories, "styles" => styles, "materials" => materials}
+	  end
   end
 
   def build_from_aggs(items, klass)
