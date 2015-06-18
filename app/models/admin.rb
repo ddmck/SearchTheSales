ac65831f1698dea45baae0eca2931f5e70827b49
Grant Leadbetter <grant@nespite.io>
2015-06-18 16:59:11 +0100

    updated admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,7 @@
 class Admin < ActiveRecord::Base
+	devise :database_authenticatable, :recoverable,
+         :trackable, :validatable, :registerable,
+         :omniauthable
+         
   include DeviseTokenAuth::Concerns::User
 end
