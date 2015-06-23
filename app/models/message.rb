0afb6af538346ec5dfbd00d0cbd8f54f558e17f5
Donald McKendrick <ddmckendrick@gmail.com>
2015-06-23 15:05:17 +0100

    add message web hook

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :text
   validates_presence_of :sender_id
+  after_create :update_ui
 
   def sender_name
     sender.name
@@ -9,5 +10,15 @@ class Message < ActiveRecord::Base
 
   def sender
     User.find(sender_id)
+  end
+
+  def update_ui
+    if sender_id != user_id
+      send_webhook
+    end
+  end
+
+  def send_webhook
+    HTTParty.post(ENV["SOCKET_URL"] + "/news", {query: {text: self.text, senderID: self.sender_id, toUser: self.user_id}})
   end
 end
