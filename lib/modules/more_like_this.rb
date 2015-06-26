4c8d47210ee8dee6729201018551631fa5b108d8
Donald McKendrick <ddmckendrick@gmail.com>
2015-06-26 17:41:02 +0100

    refactor

--- lib/modules/more_like_this.rb
+++ lib/modules/more_like_this.rb
@@ -1,13 +1,11 @@
 module MoreLikeThis
   def more_like_this
-    hash = build_more_like_this
+    hash = build_more_like_this(params["on_sale"])
     @products = Product.__elasticsearch__.search(hash).page(1).records
     respond_with(@products)
   end
 
-  def build_match_must
-    hash = {}
-    
+  def build_match_must(sale_only)
     if @product.gender_id && @product.category_id
       where_opts = {"gender_id" => @product.gender_id, "category_id" => @product.category_id}
     elsif @product.gender_id
@@ -15,15 +13,11 @@ module MoreLikeThis
     elsif @product.category_id
       where_opts = {"category_id" => @product.category_id}
     end
-    
-    where_opts = where_opts.map {|key, v| {match: {key.to_sym => v}}}
-    hash = where_opts
-    hash
+    where_opts["on_sale"] = sale_only if sale_only
+    where_opts.map {|key, v| {match: {key.to_sym => v}}}
   end
 
   def build_match_should
-    hash = {}
-
     if @product.color_id && @product.brand_id
       where_opts = {"color_id" => @product.color_id, "brand_id" => @product.brand_id}
     elsif @product.color_id
@@ -32,15 +26,12 @@ module MoreLikeThis
       where_opts = {"brand_id" => @product.brand_id}
     end
 
-    where_opts = where_opts.map {|key, v| {match: {key.to_sym => v}}}
-    hash = where_opts
-    hash
+    where_opts.map {|key, v| {match: {key.to_sym => v}}}
   end
 
-  def build_more_like_this
-    hash = {}
+  def build_more_like_this(sale_only=false)
     hash = {query: {bool: {
-              must: build_match_must,
+              must: build_match_must(sale_only),
               should: build_match_should
               }}, size: 25}
     hash
