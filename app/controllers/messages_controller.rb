5cf0accbd6ac42daf5282432577b9d94cd820809
Grant Leadbetter <grant@nespite.io>
2015-06-26 12:06:10 +0100

    authenticate_admin! added to controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin!, only: [:create_admin_message]
 
   respond_to :html, :json
 
