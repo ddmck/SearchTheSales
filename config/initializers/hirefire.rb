eebb88a1712610a77f602fdb998ae693c1e5557e
Donald McKendrick <ddmckendrick@gmail.com>
2015-07-16 08:43:36 -0400

    add endpoint for the default queue

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -6,4 +6,8 @@ HireFire::Resource.configure do |config|
   config.dyno(:worker_queue_two) do
     HireFire::Macro::Delayed::Job.queue(:indexes, :mapper => :active_record)
   end
+
+  config.dyno(:worker_queue_three) do
+    HireFire::Macro::Delayed::Job.queue(:default, :mapper => :active_record)
+  end
 end
